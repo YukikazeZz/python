9dde76fd213fb20e830a184d818c07431ed099ff
leoxu8703 <leoxu8703@gmail.com>
2017-04-02 23:10:05 +0800

    mysql workbench

---
+++ create_and_insertmysql.py
@@ -0,0 +1,66 @@
+#!/usr/local/bin/python
+from __future__ import print_function
+from contextlib import contextmanager
+import argparse
+import string
+import random
+
+import pymysql as db
+
+
+DB_NAME = 'test_insert_data_db'
+TABLE_NAME = 'test_insert_data_table'
+CREATE_TABLE_STATEMENT = """create table {0} (id int(10) NOT NULL AUTO_INCREMENT, name varchar(255) NOT NULL,datetime double NOT NULL, PRIMARY KEY (`id`))""".format(TABLE_NAME)
+
+
+def _argparse():
+    parser = argparse.ArgumentParser(description='health checker for MySQL database')
+    parser.add_argument('--host', action='store', dest='host', required=True, help='connect to host')
+    parser.add_argument('--user', action='store', dest='user', required=True, help='user for login')
+    parser.add_argument('--password', action='store', dest='password', required=True, help='password to use when connecting to server')
+    parser.add_argument('--port', action='store', dest='port', default=3306, type=int, help='port number to use for connection or 3306 for default')
+    parser.add_argument('--row_size', action='store', dest='row_size', default=5000, type=int, help='how much rows')
+    parser.add_argument('-v', '--version', action='version', version='%(prog)s 0.1')
+    return parser.parse_args()
+
+
+@contextmanager
+def get_conn(**kwargs):
+    conn=db.connect(host=kwargs.get('host', 'localhost'),user=kwargs.get('user'),passwd=kwargs.get('passwd'),port=kwargs.get('port', 3306))
+    try:
+        yield conn
+    
+    finally:
+        conn.close()
+
+
+
+def create_and_create_table(conn):
+    with conn as cur:
+        for sql in ["drop database if exists {0}".format(DB_NAME),"create database {0}".format(DB_NAME),"use {0}".format(DB_NAME),CREATE_TABLE_STATEMENT]:
+            print(sql)
+            cur.execute(sql)
+    
+def insert_data(cur):
+    source=string.ascii_letters
+    NAME="".join(random.sample(source,10))
+    insert_sql="insert into {0} (name,datetime) values('{1}',current_timestamp())".format(TABLE_NAME,NAME)
+    cur.execute(insert_sql) 
+
+def main():
+    parser = _argparse()
+    rows = parser.row_size
+    conn_args=dict(host=parser.host, user=parser.user, passwd=parser.password, port=parser.port)
+    with get_conn(**conn_args) as conn:
+        create_and_create_table(conn)
+        with conn as cur:
+            cur.execute("use test_insert_data_db")
+            for i in range(rows):
+                insert_data(cur)
+                if i % 10000 ==0:
+                    conn.commit()
+
+                  
+
+if __name__ == '__main__':
+    main()
