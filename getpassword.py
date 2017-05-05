3bf5c4f7b46464ac25f297c381b72c3abd0dc1b3
leoxu8703 <leoxu8703@gmail.com>
2017-05-05 14:07:37 +0800

    how to all hosts password

--- getpassword.py
+++ getpassword.py
@@ -1,41 +1,103 @@
 #!/usr/local/bin/python
 import socket
 from pexpect import pxssh
+import  paramiko
+import time
+from threading import Thread
 
-def trypassword(host,username,pwd,port):
-    try:                                                                            
-        s=pxssh.pxssh()                                                             
-        s.login(host,username,pwd,port=port)                                        
-        target.write("host: %s is login success,password is %s" %(host,pwd))
-        print "host: %s is login success,password is %s" %(host,pwd)
-    except:                                                                         
-        print "host: %s login has no pass" %(host)                          
-                                                                                
-        
+def write_passwd(host,username,password,port):
+    log_file='/tmp/password.txt'
+    with open(log_file,'a') as file:
+        file.write('host:%s,username:%s,password:%s,port:%s\n'%(host,username,password,port))
 
-def getpassword(host,username,pwd_dict,port):
+def write_log(host,username,curren_pwd,new_pwd):
+    log_file='/tmp/changepasswd.log'
+    with open(log_file,'a') as file:
+        file.write('host:%s,username:%s,current_password:%s,new_password:%s\n'%(host,username,curren_pwd,new_pwd))
+
+
+def trypassword(host,port,username,pwd_dict):
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    global new_pwd
+    for pwd in pwd_dict:
+        try:
+            client.connect(host,port,username=username, password=pwd)  
+            write_passwd(host,username,pwd,port)  
+            print "ip: %s using port: %s is login success,password is %s" % (host,port,pwd)   
+            print(changepassword(host,username,client,pwd,new_pwd))
+            break
+        except Exception,e:
+            print "host: %s login has no pass" %(host)
+            print (e)
+
+
+
+def getpassword(host,port,username,pwd_dict):
     sk = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sk.settimeout(1)
     result=sk.connect_ex((host,14816))
     print result
     if not result:
-        for pwd in pwd_dict:
-            trypassword(host,username,pwd,port)
+        trypassword(host,14816,username,pwd_dict)
     elif (result == 11):
-        noconnect.write("host:%s is not connect" % (host))
+        noconnect.write("host:%s is not connect\n" % (host))
         print  "host:%s is not connect" % (host)
     else:
-        for pwd in pwd_dict:
-            trypassword(host,username,pwd,port=22)     
-                
+        trypassword(host,22,username,pwd_dict)     
 
-     
+def changepassword(host,username,client,curren_pwd,new_pwd):
+    print('begin change password')
+    interact=client.invoke_shell()
+
+    interact.send('passwd'+'\n')
+    time.sleep(1)
+    print (interact.recv(65535))
+    interact.send(curren_pwd+'\n')
+    time.sleep(1)
+    print (interact.recv(65535))
+    interact.send(new_pwd+'\n')
+    time.sleep(1)
+    print (interact.recv(65535))
+    interact.send(new_pwd+'\n')
+    time.sleep(1)
+    result=interact.recv(65535).split('\r\n')[-2]
+    print (result)
+    if result=='passwd: all authentication tokens updated successfully.':
+        write_log(host,username,curren_pwd,new_pwd)
+        print('change password is successful')
+    interact.shutdown(2)
+
+
+def run_thread(func,many_thread,args):
+    threads=[]
+    for i in range(many_thread):
+        thread=Thread(target=func,args=args)
+        threads.append(thread)
+        thread.start()
+    for thread in threads:
+        thread.join() 
+
 if __name__ == '__main__':
-    pwd_dict=['leoxu8703@123','oracle2046@123','0','jingxing99@123','1qaz2wsx@123','12345.com','12345@123','ranger@123']
-    username='root'
+    username='oracle'
     port=14816
-    with open('/tmp/hosts.txt') as hosts,open('/tmp/password.txt','a') as target,open('/tmp/notconnect.txt','a') as noconnect:
+    client = paramiko.SSHClient()  
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())  
+    #client.connect('192.168.8.248', 22, username='root', password='password', timeout=4)  
+    start=time.time()
+    with open('/tmp/hosts.txt') as hosts,open('/tmp/notconnect.txt','a') as noconnect:
+        threads=[]
         for host in hosts:
             host=host.strip('\n\r')
             print "begin to get password from host:%s" %(host)
-            getpassword(host,username,pwd_dict,port)
+            #run_thread(getpassword,,args=(host,port,username,pwd_dict))
+            #getpassword(host,port,username,pwd_dict)
+            thread=Thread(target=getpassword,args=(host,port,username,pwd_dict))
+            threads.append(thread)
+            thread.start()
+
+        for thread in threads:
+            thread.join()
+    client.close()
+    end=time.time()
+    print('Finished change password is %.3f seconds'%(end - start))
